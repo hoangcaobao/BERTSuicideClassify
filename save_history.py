9ebf28afc7f3a82b082bf97f8006fdee8720bf40
Hoang Cao Bao <62338248+hoangcaobao@users.noreply.github.com>
2022-03-25 16:21:14 -0400

    Add files via upload

---
+++ save_history.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+def save_plot_and_csv(history, path_csv, path_image):
+    history_dict = history.history
+
+
+    acc = history_dict['accuracy']
+    val_acc = history_dict['val_accuracy']
+    loss = history_dict['loss']
+    val_loss = history_dict['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+    fig = plt.figure(figsize=(10, 6))
+    fig.tight_layout()
+
+    plt.subplot(2, 1, 1)
+    # r is for "solid red line"
+    plt.plot(epochs, loss, 'r', label='Training loss')
+
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    # plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+    plt.plot(epochs, acc, 'r', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend(loc='lower right')
+    plt.savefig(path_image)
+
+    pd.DataFrame.from_dict(history_dict).to_csv(path_csv,index=False)
